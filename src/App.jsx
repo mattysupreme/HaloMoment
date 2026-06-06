61d17fd2c7d78ddfa96e4b12fd49f4e58a4b47ca
mattysupreme <murdokmamat@gmail.com>
2026-06-06 18:41:06 +0700

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,7 @@ import React from 'react';
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import { LandingPage } from './presentation/pages/LandingPage';
 import { Dashboard } from './presentation/pages/dashboard';
+import { Analytics } from "@vercel/analytics/next"
 
 function App() {
   return (
