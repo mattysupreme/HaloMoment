20b57c43664f341379499f34286d3d129cedcf8e
mattysupreme <murdokmamat@gmail.com>
2026-06-06 18:42:46 +0700

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -2,11 +2,12 @@ import React from 'react';
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import { LandingPage } from './presentation/pages/LandingPage';
 import { Dashboard } from './presentation/pages/dashboard';
-import { Analytics } from "@vercel/analytics/next"
+import { Analytics } from "@vercel/analytics/react";
 
 function App() {
   return (
     <BrowserRouter>
+      <Analytics />
       <Routes>
         <Route path="/" element={<LandingPage />} />
         <Route path="/dashboard" element={<Dashboard />} />
