d9b3678bac83390177c7804bdb9040f0b81306bc
mattysupreme <murdokmamat@gmail.com>
2026-06-30 09:39:39 +0700

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,7 @@ import React from 'react';
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import { LandingPage } from './presentation/pages/LandingPage';
 import { Dashboard } from './presentation/pages/dashboard';
+import { CustomerDashboard } from './presentation/pages/CustomerDashboard';
 import { Analytics } from "@vercel/analytics/react";
 
 function App() {
@@ -11,6 +12,7 @@ function App() {
       <Routes>
         <Route path="/" element={<LandingPage />} />
         <Route path="/dashboard" element={<Dashboard />} />
+        <Route path="/customer" element={<CustomerDashboard />} />
       </Routes>
     </BrowserRouter>
   );
